d8cdd9611efde1c4df14953b64e6d0103366c188
jlx321@126.com <jlx321@126.com>
2024-03-20 19:52:45 +0800

    [#8] DataBus: Implement databus

---
+++ DataBus.cpp
@@ -0,0 +1,34 @@
+#include "DataBus.h"
+
+#include <iostream>
+
+abc::DataBus& abc::DataBus::instance()
+{
+    static abc::DataBus instance;
+    return instance;
+}
+
+sqlite3* abc::DataBus::openDatabase()
+{
+    if (abc::DataBus::instance().db != nullptr)
+    {
+        return abc::DataBus::instance().db;
+    }
+
+    if (sqlite3_open("account_book.db", &abc::DataBus::instance().db) != SQLITE_OK)
+    {
+        std::cerr << "Can't open database: " << sqlite3_errmsg(abc::DataBus::instance().db) << std::endl;
+    }
+    else
+    {
+        std::cout << "Opened database successfully" << std::endl;
+    }
+
+    return abc::DataBus::instance().db;
+}
+
+void abc::DataBus::closeDatabase()
+{
+    sqlite3_close(abc::DataBus::instance().db);
+    abc::DataBus::instance().db = nullptr;
+}
